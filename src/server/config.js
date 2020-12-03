d90fdc4e74aee13dc2e46afa1c77b71dc0749ef6
Enijar <TheEnijar@gmail.com>
2020-12-03 23:05:33 +0000

    chore: added client

--- src/server/config.js
+++ src/server/config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const basePath = path.resolve(__dirname, "..", "..");
 
 module.exports = {
-  port: process?.env?.PORT ?? 3000,
+  port: process?.env?.PORT ?? 8080,
   host: process?.env?.HOST ?? "localhost",
   dbFile: process?.env?.DB_FILE ?? "storage/db.json",
   paths: {
